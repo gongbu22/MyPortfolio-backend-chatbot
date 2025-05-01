6968f776e9c642fbe99b3e8e26a2e204a4a7a94b
gongbu22 <pyujin0711@naver.com>
2025-05-01 17:28:16 +0900

    수정

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers.chatbot import router as chatbot_router
-from dotenv import load_dotenv
-import os
+# from dotenv import load_dotenv
+# import os
 
 app = FastAPI()
 
-react_host = os.getenv("REACT_APP_HOST", "http://localhost")
-react_port = os.getenv("REACT_APP_PORT", "3000")
+# react_host = os.getenv("REACT_APP_HOST", "http://localhost")
+# react_port = os.getenv("REACT_APP_PORT", "3000")
 
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[f"{react_host}:{react_port}"],  # React 앱이 동작하는 주소 (개발 중)
+    allow_origins=["http://frontend-service"],  # React 앱이 동작하는 주소 (개발 중)
     allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 허용
     allow_headers=["*"],  # 모든 헤더 허용
